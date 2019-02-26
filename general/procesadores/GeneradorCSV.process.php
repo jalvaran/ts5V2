d2db06d2edb20a99257ab5abec24f2c209f045b5
JULIAN <JULIAN@192.168.1.67>
2019-02-26 17:38:53 -0500

    Se configura para que exporte con la asocian de campos desde la base de datos

--- general/procesadores/GeneradorCSV.process.php
+++ general/procesadores/GeneradorCSV.process.php
@@ -36,6 +36,14 @@ if(isset($_REQUEST["Opcion"])){
             $Separador=$obCon->normalizar($_REQUEST["Separador"]);
             
             $ColumnasSeleccionadas=$obCon->getColumnasVisibles($Tabla, "");  
+            
+            $DatosConsulta=$obCon->getConsultaTabla($Tabla,$ColumnasSeleccionadas, $Condicion, $OrdenColumna, $AscDesc, $NumPage, $limit,$startpoint);
+           
+            $TotalRegistros=$DatosConsulta["TotalRegistros"];
+            $QueryCompleto=$DatosConsulta["QueryCompleto"];
+            $QueryParcial=$DatosConsulta["QueryParcial"];
+            
+            
             $idTabla=$ColumnasSeleccionadas["Field"][0];        
             if($Condicion<>""){
                 $Condicion=" WHERE ".$Condicion;
@@ -60,7 +68,9 @@ if(isset($_REQUEST["Opcion"])){
             $sqlColumnas.=" UNION ALL ";
             $Indice=$ColumnasSeleccionadas["Field"][0];
             
-            $sql=$sqlColumnas."SELECT $CamposShow FROM $Tabla $Condicion INTO OUTFILE '$OuputFile' FIELDS TERMINATED BY '$Separador' $Enclosed LINES TERMINATED BY '\r\n';";
+            //$sql=$sqlColumnas."SELECT $CamposShow FROM $Tabla $Condicion INTO OUTFILE '$OuputFile' FIELDS TERMINATED BY '$Separador' $Enclosed LINES TERMINATED BY '\r\n';";
+            $sql=$sqlColumnas."$QueryParcial $Condicion INTO OUTFILE '$OuputFile' FIELDS TERMINATED BY '$Separador' $Enclosed LINES TERMINATED BY '\r\n';";
+            
             $obCon->Query($sql);
             print("<div id='DivImagenDescargarTablaDB'><a href='$Link' download='$Tabla.csv' target='_top' style='text-align:center;position: absolute;top:50%;left:50%;padding:5px;' onclick=document.getElementById('DivImagenDescargarTablaDB').style.display='none';><h1>Descargar: </h1><img src='../../images/descargar3.png'></img></a></div>");
             break;
